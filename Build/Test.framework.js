9b9654c3e6de145f134cecc6d1fddc35d48b9ca5
Kelton <95721065+codeninjaskelton@users.noreply.github.com>
2024-01-23 17:32:06 -0500

    build18

--- Build/Test.framework.js
+++ Build/Test.framework.js
@@ -1159,26 +1159,26 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 2245572: function() {
+ 2245620: function() {
   Module["emscripten_get_now_backup"] = performance.now;
- },
- 2245627: function($0) {
-  performance.now = function() {
-   return $0;
-  };
  },
  2245675: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 2245723: function() {
+ 2245723: function($0) {
+  performance.now = function() {
+   return $0;
+  };
+ },
+ 2245771: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 2245778: function() {
+ 2245826: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 2245839: function() {
+ 2245887: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  }
 };
@@ -14217,21 +14217,10 @@ function invoke_vii(index, a1, a2) {
  }
 }
 
-function invoke_vi(index, a1) {
+function invoke_iiiiii(index, a1, a2, a3, a4, a5) {
  var sp = stackSave();
  try {
-  dynCall_vi(index, a1);
- } catch (e) {
-  stackRestore(sp);
-  if (e !== e + 0 && e !== "longjmp") throw e;
-  _setThrew(1, 0);
- }
-}
-
-function invoke_viii(index, a1, a2, a3) {
- var sp = stackSave();
- try {
-  dynCall_viii(index, a1, a2, a3);
+  return dynCall_iiiiii(index, a1, a2, a3, a4, a5);
  } catch (e) {
   stackRestore(sp);
   if (e !== e + 0 && e !== "longjmp") throw e;
@@ -14250,10 +14239,10 @@ function invoke_iii(index, a1, a2) {
  }
 }
 
-function invoke_iiiiii(index, a1, a2, a3, a4, a5) {
+function invoke_viii(index, a1, a2, a3) {
  var sp = stackSave();
  try {
-  return dynCall_iiiiii(index, a1, a2, a3, a4, a5);
+  dynCall_viii(index, a1, a2, a3);
  } catch (e) {
   stackRestore(sp);
   if (e !== e + 0 && e !== "longjmp") throw e;
@@ -14309,6 +14298,17 @@ function invoke_diii(index, a1, a2, a3) {
  var sp = stackSave();
  try {
   return dynCall_diii(index, a1, a2, a3);
+ } catch (e) {
+  stackRestore(sp);
+  if (e !== e + 0 && e !== "longjmp") throw e;
+  _setThrew(1, 0);
+ }
+}
+
+function invoke_vi(index, a1) {
+ var sp = stackSave();
+ try {
+  dynCall_vi(index, a1);
  } catch (e) {
   stackRestore(sp);
   if (e !== e + 0 && e !== "longjmp") throw e;
